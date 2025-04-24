16775f7fdad11415909a58b910677bcabfc07494
Павел <triplpridurok@spcs.me>
2025-04-25 02:54:26 +0300

    src add

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -7,7 +7,7 @@ const Index = () => {
   return (
     <MainLayout>
       <div className={styles.hero}>
-      <img src="http://localhost:5000/image/obl.jpg" alt="SoundNest Cover" className={styles.heroImg}/>
+      <img src="https://servermp-production.up.railway.app/image/obl.jpg" alt="SoundNest Cover" className={styles.heroImg}/>
         <h1>Добро пожаловать в <span>SoundNest</span>!</h1>
         <h3>Исследуйте, создавайте и наслаждайтесь лучшей музыкой со всего мира.</h3>
         <p>SoundNest — это место, где музыка оживает. Открывайте новые треки, делитесь своими плейлистами и находите вдохновение в ритме.</p>
