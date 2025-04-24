60877b474e42893451934cd2e570735895cfbad9
Павел <triplpridurok@spcs.me>
2025-04-25 01:24:19 +0300

    Общее

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,13 +5,13 @@ const start = async () => {
     try {
         const PORT = process.env.PORT || 5000;
         const app = await NestFactory.create(AppModule);
-        
+
         app.enableCors({
-            origin: ['*'],
+            origin: ['https://my-frontend.onrender.com'], // или '*', но лучше точный домен
             methods: ['GET', 'POST', 'DELETE'],
         });
 
-        await app.listen(PORT, () => console.log(`server started on PORT ${PORT}`));
+        await app.listen(PORT, '0.0.0.0', () => console.log(`server started on PORT ${PORT}`));
     } catch (e) {
         console.log(e);
     }
